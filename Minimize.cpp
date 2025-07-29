d7457714832d9e8676bc387c774a517358f2faf3
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2025-07-29 22:55:49 +0300

    Create Minimize.cpp

---
+++ Minimize.cpp
@@ -0,0 +1,20 @@
+// problem A Round 971 (Div. 4)
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int a , b;
+        cin >> a >> b;
+        cout << b - a << endl;
+    }
+ 
+    return 0;
+}
+
+//````the link : https://codeforces.com/problemset/problem/2009/A
